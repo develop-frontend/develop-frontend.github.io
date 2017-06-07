8474efa54b5b72af6de966fb193ea784eb01487e
develop-frontend <dmytro.group@gmail.com>
2017-06-07 13:41:36 +0300

    Update page_active.js

--- GLUTON/js/page_active.js
+++ GLUTON/js/page_active.js
@@ -1,1 +1,4 @@
-$(document).ready(function() {   var activPage =  $('body').data('page');   var activMenu = '.nav_menu_' + activPage;   $(activMenu).addClass('nav_menu_page_activ');});
+$(document).ready(function() {   
+  var activPage =  $('body').data('page');   
+  var activMenu = '.nav_menu_' + activPage;   
+  $(activMenu).addClass('nav_menu_page_activ');});
