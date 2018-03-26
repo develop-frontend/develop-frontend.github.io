cbd6df2e7869bde8af8da01f8eed78a765b6db17
develop-frontend <dmytro.group@gmail.com>
2018-03-26 13:00:47 +0300

    new

--- adminPanel/js/script.js
+++ adminPanel/js/script.js
@@ -1,6 +1,14 @@
 // $(document).ready(function () {
 // });
 
-    function showAside() {
-        $('.wrapper aside').toggleClass('active');
-    };
+function showAside() {
+    $('.wrapper aside').toggleClass('active');
+};
+
+function tab(a){
+	var _i = a.currentTarget.getAttribute('data-id'),
+		_t = a.target,
+		_a = _t.getAttribute('data-tab');
+	$('div[data-id='+_i+'], ul[data-id='+_i+'] > li').removeClass('active');
+	$('li[data-tab='+_a+'], #'+_a).addClass('active');
+};
